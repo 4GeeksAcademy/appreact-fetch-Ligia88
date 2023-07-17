66915992c8c06dd5cce8780ac7f7759128daa87c
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-17 21:57:20 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,20 +5,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const pokeApi = 'https://pokeapi.co/api/v2/';
+
+	//fetch(urlAPI).then(promesa).then(resultado).catch(mesaje de error)
+	fetch(pokeApi).then((result) => {
+		console.log (result)
+		return result.json()
+		}).then((data)=> {
+		return console.log(data)
+		}).catch()
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center">	
 		</div>
 	);
 };
